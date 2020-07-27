198c01c4356b3f2240566a445d167edd74a81755
ValterSilvaJr <valtersilvajr@live.com>
2020-07-27 18:33:33 -0300

    criação dos components restantes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import dadosIniciais from './data/dados_iniciais.json';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!!</h1>
+      <Menu />
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem afinal? Descubra com a Vanessa!"}
+      />
+
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel 
+        category={dadosIniciais.categorias[5]}
+      />
+
     </div>
   );
 }
